d94296f47ee8a3c3c276feba94341a627f6d56b6
Chen Keinan <Hen.Keinan@gmail.com>
2021-10-09 10:25:51 +0300

    ffactor: update match policy method signature

--- validator/policyeval_test.go
+++ validator/policyeval_test.go
@@ -16,11 +16,11 @@ func Test_PolicyEval(t *testing.T) {
 		want       bool
 		wantError  error
 	}{
-		{name: "test validate policy deny pod name json format", data: "./fixture/pod.json", pkgName: "example", policyRule: []string{"deny"}, policy: "./fixture/pod_policy_deny", want: true, wantError: nil},
-		{name: "test validate policy deny pod name yaml format", data: "./fixture/pod.yaml", pkgName: "example", policyRule: []string{"deny"}, policy: "./fixture/pod_policy_deny", want: true, wantError: nil},
-		{name: "test validate policy allow pod name", data: "./fixture/allow_pod.json", pkgName: "example", policyRule: []string{"deny"}, policy: "./fixture/pod_policy_deny", want: false, wantError: nil},
-		{name: "test validate policy bad data", data: "./fixture/badJson.json", pkgName: "example", policyRule: []string{"deny"}, policy: "./fixture/pod_policy_deny", want: false, wantError: nil},
-		{name: "test validate policy bad policy", data: "./fixture/badJson.json", pkgName: "example", policyRule: []string{"deny"}, policy: "./fixture/pod_policy_deny_bad", want: false, wantError: fmt.Errorf("1 error occurred: .rego:5: rego_parse_error: unexpected } token\n\t}\n\t^")},
+		{name: "test validate policy deny pod name json format", data: "./fixture/pod.json", policyRule: []string{"example.deny"}, policy: "./fixture/pod_policy_deny", want: true, wantError: nil},
+		{name: "test validate policy deny pod name yaml format", data: "./fixture/pod.yaml", policyRule: []string{"example.deny"}, policy: "./fixture/pod_policy_deny", want: true, wantError: nil},
+		{name: "test validate policy allow pod name", data: "./fixture/allow_pod.json", policyRule: []string{"example.deny"}, policy: "./fixture/pod_policy_deny", want: false, wantError: nil},
+		{name: "test validate policy bad data", data: "./fixture/badJson.json", policyRule: []string{"example.deny"}, policy: "./fixture/pod_policy_deny", want: false, wantError: nil},
+		{name: "test validate policy bad policy", data: "./fixture/badJson.json", policyRule: []string{"example.deny"}, policy: "./fixture/pod_policy_deny_bad", want: false, wantError: fmt.Errorf("1 error occurred: eval.rego:5: rego_parse_error: unexpected } token\n\t}\n\t^")},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -32,41 +32,17 @@ func Test_PolicyEval(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			got, err := NewPolicyEval().EvaluatePolicy([]string{"deny"}, string(policy), string(data))
+			got, err := NewPolicyEval().EvaluatePolicy(tt.policyRule, string(policy), string(data))
 			if err != nil {
 				goErr := err.Error()
 				if goErr != tt.wantError.Error() {
 					t.Fatal(err)
 				}
 			}
-			if len(got) > 0 {
+			if err == nil {
 				if got[0].Value != tt.want {
 					t.Errorf("Test_PolicyEval() = %v, want %v", got[0], tt.want)
 				}
-			}
-		})
-	}
-}
-
-func Test_DetectPkgName(t *testing.T) {
-	tests := []struct {
-		name   string
-		policy string
-		want   string
-	}{
-		{name: "detect pkg name exist", policy: "./fixture/pod_policy_deny", want: "example"},
-		{name: "detect pkg name not exist", policy: "./fixture/pod_policy_deny_bad", want: ""},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			policy, err := ioutil.ReadFile(tt.policy)
-			if err != nil {
-				t.Fatal(err)
-			}
-			pe := &policyEval{}
-			got := pe.detectPkgName(string(policy))
-			if got != tt.want {
-				t.Errorf("Test_DetectPkgName() = %v, want %v", got[0], tt.want)
 			}
 		})
 	}
