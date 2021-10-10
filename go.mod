3dc527453599ac141eadebec16100141e5dc7720
Chen Keinan <Hen.Keinan@gmail.com>
2021-10-10 20:16:07 +0300

    feat: validate expr vith policy

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/chen-keinan/go-opa-validate
 go 1.16
 
 require (
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/open-policy-agent/opa v0.33.1
 	gopkg.in/yaml.v2 v2.4.0
 )
--- validator/policyeval_test.go
+++ validator/policyeval_test.go
@@ -21,6 +21,7 @@ func Test_PolicyEval(t *testing.T) {
 		{name: "test validate policy allow pod name", data: "./fixture/allow_pod.json", policyRule: []string{"example.deny"}, policy: "./fixture/pod_policy_deny", want: false, wantError: nil},
 		{name: "test validate policy bad data", data: "./fixture/badJson.json", policyRule: []string{"example.deny"}, policy: "./fixture/pod_policy_deny", want: false, wantError: nil},
 		{name: "test validate policy bad policy", data: "./fixture/badJson.json", policyRule: []string{"example.deny"}, policy: "./fixture/pod_policy_deny_bad", want: false, wantError: fmt.Errorf("1 error occurred: eval.rego:5: rego_parse_error: unexpected } token\n\t}\n\t^")},
+		{name: "test validate policy bad policy", data: "./fixture/strict_policy.json", policyRule: []string{"itsio.allow"}, policy: "./fixture/deny_strict.policy", want: true, wantError: nil},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
