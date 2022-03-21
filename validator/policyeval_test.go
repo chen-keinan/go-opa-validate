5d158440f2c89d5afcc7bf5e31174af615dde017
chenk <hen.keinan@gmail.com>
2022-03-21 15:55:20 +0200

    feat: auto detect pkg name
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- validator/policyeval_test.go
+++ validator/policyeval_test.go
@@ -17,12 +17,12 @@ func Test_PolicyEval(t *testing.T) {
 		want       interface{}
 		wantError  error
 	}{
-		{name: "test validate policy deny pod name json format", data: "./fixture/pod.json", policyRule: []string{"example.deny"}, policy: "./fixture/pod_policy_deny", want: true, wantError: nil},
-		{name: "test validate policy deny pod name yaml format", data: "./fixture/pod.yaml", policyRule: []string{"example.deny"}, policy: "./fixture/pod_policy_deny", want: true, wantError: nil},
-		{name: "test validate policy allow pod name", data: "./fixture/allow_pod.json", policyRule: []string{"example.deny"}, policy: "./fixture/pod_policy_deny", want: false, wantError: nil},
-		{name: "test validate policy bad data", data: "./fixture/badJson.json", policyRule: []string{"example.deny"}, policy: "./fixture/pod_policy_deny", want: false, wantError: nil},
-		{name: "test validate policy bad policy", data: "./fixture/badJson.json", policyRule: []string{"example.deny"}, policy: "./fixture/pod_policy_deny_bad", want: false, wantError: fmt.Errorf("1 error occurred: eval.rego:5: rego_parse_error: unexpected } token\n\t}\n\t^")},
-		{name: "test validate policy bad policy", data: "./fixture/strict_policy.json", policyRule: []string{"itsio.allow"}, policy: "./fixture/deny_strict.policy", want: map[string]interface{}{"allow_policy": true, "name": "foo"}, wantError: nil},
+		{name: "test validate policy deny pod name json format", data: "./fixture/pod.json", policyRule: []string{"deny"}, policy: "./fixture/pod_policy_deny", want: true, wantError: nil},
+		{name: "test validate policy deny pod name yaml format", data: "./fixture/pod.yaml", policyRule: []string{"deny"}, policy: "./fixture/pod_policy_deny", want: true, wantError: nil},
+		{name: "test validate policy allow pod name", data: "./fixture/allow_pod.json", policyRule: []string{"deny"}, policy: "./fixture/pod_policy_deny", want: false, wantError: nil},
+		{name: "test validate policy bad data", data: "./fixture/badJson.json", policyRule: []string{"deny"}, policy: "./fixture/pod_policy_deny", want: false, wantError: nil},
+		{name: "test validate policy bad policy", data: "./fixture/badJson.json", policyRule: []string{"deny"}, policy: "./fixture/pod_policy_deny_bad", want: false, wantError: fmt.Errorf("1 error occurred: eval.rego:5: rego_parse_error: unexpected } token\n\t}\n\t^")},
+		{name: "test validate policy bad policy", data: "./fixture/strict_policy.json", policyRule: []string{"allow"}, policy: "./fixture/deny_strict.policy", want: map[string]interface{}{"allow_policy": true, "name": "foo"}, wantError: nil},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
